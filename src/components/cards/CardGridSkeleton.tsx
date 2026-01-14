09adcabcce850d0b01befb23a00c487cf9337365
lennking <vikkerz456@gmail.com>
2026-01-14 14:53:39 -0800

    perf(sets): Remove redundant hydration wait and fix layout shifts

--- src/components/cards/CardGridSkeleton.tsx
+++ src/components/cards/CardGridSkeleton.tsx
@@ -2,8 +2,8 @@ import { PokemonCardSkeleton } from './PokemonCardSkeleton';
 
 export function CardGridSkeleton() {
     return (
-        <div className='min-h-screen flex-grow'>
-            <div className='grid grid-cols-2 gap-4 px-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+        <div className='flex-grow'>
+            <div className='grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
                 {Array.from({ length: 20 }).map((_, i) => (
                     <PokemonCardSkeleton key={i} />
                 ))}
