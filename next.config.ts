f6fae92340a7a3bb1a193b2dedb66d7212c01074
lennking <vikkerz456@gmail.com>
2026-01-14 21:44:27 -0800

    Revert "perf(cache): Aggressively cache for instant navigation"
    This reverts commit 807ccaaf75e8380607728d3d5c958b72f91eab56.

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
-    experimental: {
-        staleTimes: {
-            dynamic: 300, // Keep dynamic card pages in browser memory for 5 mins
-            static: 1800 // Keep pre-rendered pages for 30 mins
-        }
-    },
     images: {
         loader: 'custom',
         loaderFile: './src/lib/loader.ts'
